4e972ad761416c9c2d4d6f15a8555978d4d5ddd5
Алена Кудрявцева <sunny_aga@mail.ru>
2022-10-31 22:24:57 +0300

    tests

---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,35 @@
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    private static Faker getFaker() {
+        return  new Faker(new Locale("ru"));
+    }
+
+    public static LocalDate generateDate() {
+        long minDay = LocalDate.now().plusDays(1).toEpochDay();
+        long maxDay = LocalDate.now().plusMonths(2).toEpochDay();
+        long someDay = ThreadLocalRandom.current().nextLong(minDay,maxDay);
+        return LocalDate.ofEpochDay(someDay);
+    }
+    public static String generateCity() {
+        return DataGenerator.getFaker().address().city();
+    }
+
+    public static String generateName() {
+        return DataGenerator.getFaker().name().fullName();
+    }
+
+    public static String generatePhone() {
+        return DataGenerator.getFaker().phoneNumber().phoneNumber();
+    }
+
+
+}
+
